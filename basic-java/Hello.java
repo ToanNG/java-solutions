6a2ff2bd4a107c13a7f90971a4a9c4c633a3a46f
Toan Nguyen <toan2406@gmail.com>
2017-01-23 14:48:13 +0700

    add Hello example

---
+++ basic-java/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+  public String say(String name) {
+    String result = "Hello, " + name + "!";
+    return result;
+  }
+
+  public String say() {
+    String result = "Hello, world!";
+    return result;
+  }
+}
