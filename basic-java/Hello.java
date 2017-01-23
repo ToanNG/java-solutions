318b82467ab19554ea31865c931393eac5a8ecbf
Toan Nguyen <toan2406@gmail.com>
2017-01-23 15:20:24 +0700

    change answer

--- basic-java/Hello.java
+++ basic-java/Hello.java
@@ -1,3 +1,11 @@
 public class Hello {
-  
+  public String say(String name) {
+    String result = "Hello, " + name + "!";
+    return result;
+  }
+
+  public String say() {
+    String result = "Hello, world";
+    return result;
+  }
 }
