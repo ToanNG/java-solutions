0ff1e3826862d3cd4724e27db176c2a4037d5ee4
Toan Nguyen <toan2406@gmail.com>
2017-02-07 11:05:10 +0700

    add more test

---
+++ basic-java/variable/BookCart.java
@@ -0,0 +1,15 @@
+/*
+Tiara membeli buku di sebuah toko buku besar di kota Bandung. Karena Tiara menyukai beberapa jenis buku,
+maka dia membeli buku lebih dari satu. Buku yang dia beli yakni, biografi 2 buah, majalah 1 buah,
+novel 2 buah, ensiklopedia 4 buah, dan komik 3 buah.
+
+Lengkapi baris program dibawah ini untuk memenuhi keterangan di atas!
+*/
+public class BookCart {
+  // Put your answers below
+  int bukuBiografi = 2;
+  int bukuMajalah = 1;
+  int bukuNovel = 2;
+  int bukuEnsiklopedia = 4;
+  int bukuKomik = 5;
+}
